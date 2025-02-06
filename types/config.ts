ed97c401881e4c673beeaee448fdd42c4fd6de36
maxMODEbr <andrei.barros77@gmail.com>
2025-02-06 01:49:08 -0300

    Update config.ts

--- types/config.ts
+++ types/config.ts
@@ -31,7 +31,7 @@ export type Config = {
   shortName: string;
   description: string;
   direction?: 'auto' | 'ltr' | 'rtl';
-  language?: 'en-US';
+  language?: 'en-US' | 'pt-BR';
   backgroundColor?: `#${string}`;
   themeColor?: `#${string}`;
   display?: 'fullscreen' | 'standalone' | 'minimal-ui' | 'browser';
